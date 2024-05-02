5b7c962ea250239905e65155c36a44d7c72a78e9
admin1622 <168673167+admin1622@users.noreply.github.com>
2024-05-03 04:31:49 +0530

    Update app.js

--- node_ practical/app.js
+++ node_ practical/app.js
@@ -2,6 +2,18 @@ const express=require('express');
 const app=express();
 const port=3000;
 
+/*const express=require('express');
+const path=require('path');
+const port=4000;
+const app=express();
+
+const publicDirectory=path.join(__dirname,"public");
+app.use(express.static(publicDirectory));
+
+app.listen(port,(req,res)=>{
+    console.log(`Listening on port ${port}`);
+});*/
+
 app.get('/',(req,res)=>{
     res.sendFile('/code/index.html', {root: __dirname});
 });
